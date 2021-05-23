1e4d15060d2f1af69f16a6ca5e9009954400fdff
Jonathan Laferen <60574415+jonathanmarp@users.noreply.github.com>
2021-05-23 21:09:47 +0700

    Commit #00018 (Fix Error)
    example/adding_data.cpp: In function ‘int main(int, const char**)’:
    example/adding_data.cpp:11:36: error: expected ‘;’ before ‘std’
       11 |   db.adding_string("Hello, World!") // id = 4
          |                                    ^
          |                                    ;
       12 | 
       13 |   std::cout << db.get_number<int>(1) << std::endl;    // output = 10
          |   ~~~                               
    Error: Process completed with exit code 1.

--- example/adding_data.cpp
+++ example/adding_data.cpp
@@ -8,7 +8,7 @@ int main(int argc, const char* argv[]) {
   db.adding_number<int>(10);        // id = 1
   db.adding_number<float>(20.3f);   // id = 2
   db.adding_number<double>(34.234); // id = 3
-  db.adding_string("Hello, World!") // id = 4
+  db.adding_string("Hello, World!"); // id = 4
   
   std::cout << db.get_number<int>(1) << std::endl;    // output = 10
   std::cout << db.get_number<float>(2) << std::endl;  // output = 20.3
