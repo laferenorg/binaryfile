d49d2cc31940201fb8a2decb2ed13205086580eb
Jonathan Laferen <60574415+jonathanmarp@users.noreply.github.com>
2021-05-23 21:11:39 +0700

    Commit #00020
    example/read_into_memory.cpp: In function ‘int main(int, const char**)’:
    example/read_into_memory.cpp:9:16: error: ‘myFile’ was not declared in this scope
        9 |   std::cout << myFile.length() << std::endl; // if the file empty this give 0 length
          |                ^~~~~~
    Error: Process completed with exit code 1.

--- example/read_into_memory.cpp
+++ example/read_into_memory.cpp
@@ -6,7 +6,7 @@ int main(int argc, const char* argv[]) {
   std::binaryfile db = "data.db";
   db.init(); // read file into memory
   
-  std::cout << myFile.length() << std::endl; // if the file empty this give 0 length
+  std::cout << db.length() << std::endl; // if the file empty this give 0 length
   {
     (void)argc;
     (void)argv;
